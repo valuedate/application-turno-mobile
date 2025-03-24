6eb1e2d3b398e878c7fb531c5d3a10393dc2308c
Ricardo Fernandes Timestamp <RicardoFernandes.Timestamp@edp.com>
2025-03-24 16:29:44 +0000

    new customlocationpermission

--- lib/utils/geolocator_helper.dart
+++ lib/utils/geolocator_helper.dart
@@ -16,6 +16,7 @@ class GeolocatorHelper {
         timestamp: DateTime.now(),
         accuracy: 0,
         altitude: 0,
+        altitudeAccuracy: 0,
         heading: 0,
         speed: 0,
         speedAccuracy: 0,
